4df77397444ee727df8513c94377f804399c4bc8
Wisker <TheWisker@protonmail.com>
2025-07-25 16:59:10 +0200

    Fixed snapcraft issues with arquitectures & now flatpak script runs test instance

--- scripts/make_flatpak.sh
+++ scripts/make_flatpak.sh
@@ -2,4 +2,5 @@
 VERSION=$(grep -oP "(?<!meson_)version:\s*'\K[^']+" meson.build)
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak-builder --user --force-clean --install-deps-from=flathub --repo=repo --install builddir io.github.TheWisker.Cavasik.json
-flatpak build-bundle ./repo cavasik_${VERSION}_all.flatpak io.github.TheWisker.Cavasik
+flatpak build-bundle ./repo cavasik_${VERSION}_amd64.flatpak io.github.TheWisker.Cavasik
+flatpak-builder --run builddir io.github.TheWisker.Cavasik.json cavasik
--- scripts/make_snap.sh
+++ scripts/make_snap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+VERSION=$(grep -oP "(?<!meson_)version:\s*'\K[^']+" meson.build)
 snapcraft clean
 snapcraft --debug --verbose
 sudo snap remove cavasik
-sudo snap install --dangerous cavasik*.snap
+sudo snap install --dangerous cavasik_${VERSION}_amd64.snap
 sudo systemctl restart snapd
 sudo systemctl daemon-reload
 sudo snap run cavasik || snap run cavasik
